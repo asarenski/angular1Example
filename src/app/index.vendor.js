407cce10e00a9dc6aa30b399b971f94735618b5a
asarenski <asarenski@gmail.com>
2017-03-18 15:14:24 -0400

    yeoman gen for angular, es6, webpack

---
+++ src/app/index.vendor.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// node_modules
+import "angular";
+
+import "angular-ui-router";
+
+
+import "angular-sanitize";
+
+import "jquery";
+
+
+
+// local scripts
+//import "../assets/js/...";
