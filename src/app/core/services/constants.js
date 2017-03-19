6d567bd3813748f06e0aa878fd62999544b20aee
asarenski <asarenski@gmail.com>
2017-03-19 17:17:39 -0400

    registered timestamps conform with iso format

--- src/app/core/services/constants.js
+++ src/app/core/services/constants.js
@@ -23,7 +23,7 @@ export default function (app) {
                         "phone": "+1 (948) 490-2142",
                         "address": "919 Franklin Avenue, Elfrida, Florida, 7961",
                         "about": "Eu do enim pariatur irure occaecat Lorem esse laborum. Proident consequat in excepteur do reprehenderit mollit nostrud. Aute id officia fugiat labore qui consequat. Non cillum commodo esse dolor aliquip ea excepteur cupidatat officia proident. Aliqua id in non pariatur id amet velit ullamco labore ullamco consequat.\r\n",
-                        "registered": "2015-09-21T06:16:17 +04:00",
+                        "registered": "2015-09-21T06:16:17+04:00",
                         "latitude": -33.443338,
                         "longitude": 84.484635,
                         "tags": [
@@ -68,7 +68,7 @@ export default function (app) {
                         "phone": "+1 (861) 447-3869",
                         "address": "707 Balfour Place, Twilight, California, 820",
                         "about": "Pariatur adipisicing magna nisi ad. Incididunt nisi dolore culpa amet dolore. Aliqua veniam irure tempor nisi cillum deserunt do aliquip excepteur elit non. Eu amet sunt mollit laboris culpa mollit esse. Anim minim reprehenderit consequat mollit ullamco commodo. Dolor culpa sint nisi voluptate veniam enim adipisicing aliqua veniam qui ut ut do in.\r\n",
-                        "registered": "2015-01-19T02:57:32 +05:00",
+                        "registered": "2015-01-19T02:57:32+05:00",
                         "latitude": 50.982171,
                         "longitude": 108.219808,
                         "tags": [
@@ -113,7 +113,7 @@ export default function (app) {
                         "phone": "+1 (932) 514-3535",
                         "address": "221 Ferry Place, Machias, Kansas, 6209",
                         "about": "Quis enim exercitation sit anim. Et eu incididunt ullamco ex enim fugiat ex in sunt aliquip sit minim. Reprehenderit pariatur enim reprehenderit nulla ea dolore nostrud est consectetur reprehenderit do labore aliqua nulla.\r\n",
-                        "registered": "2016-11-17T06:08:29 +05:00",
+                        "registered": "2016-11-17T06:08:29+05:00",
                         "latitude": 52.307695,
                         "longitude": 76.479773,
                         "tags": [
@@ -158,7 +158,7 @@ export default function (app) {
                         "phone": "+1 (810) 445-2365",
                         "address": "779 Legion Street, Hayden, Ohio, 6275",
                         "about": "Lorem ex adipisicing cillum Lorem aute sunt voluptate amet irure laborum. Voluptate anim laboris sit ut labore nisi ullamco. Eiusmod enim voluptate mollit ut ipsum elit. In culpa aliqua ipsum ullamco reprehenderit consequat commodo Lorem pariatur mollit in consequat quis quis.\r\n",
-                        "registered": "2015-10-07T09:46:45 +04:00",
+                        "registered": "2015-10-07T09:46:45+04:00",
                         "latitude": -22.83806,
                         "longitude": -106.611789,
                         "tags": [
@@ -203,7 +203,7 @@ export default function (app) {
                         "phone": "+1 (984) 497-3633",
                         "address": "613 Eldert Lane, Westboro, Guam, 5391",
                         "about": "Duis ea aliquip sunt sunt do officia exercitation occaecat ut. Amet et ullamco laboris ea est tempor ad aliqua id quis magna amet fugiat. Id enim deserunt sit ad ea. Amet in excepteur mollit culpa magna nisi pariatur. Dolor eu nisi Lorem aute occaecat minim.\r\n",
-                        "registered": "2014-10-27T05:37:01 +04:00",
+                        "registered": "2014-10-27T05:37:01+04:00",
                         "latitude": 0.480938,
                         "longitude": -124.539209,
                         "tags": [
@@ -248,7 +248,7 @@ export default function (app) {
                         "phone": "+1 (825) 449-2351",
                         "address": "944 Rapelye Street, Montura, Virginia, 9116",
                         "about": "Magna occaecat nostrud duis irure mollit aute aliqua et. Nostrud cillum ut et velit excepteur exercitation labore ad id consequat aliqua quis veniam. Eu ex Lorem ex aliqua ad laboris ea sit nisi cupidatat magna duis. Lorem aliquip ea sint id sit veniam nulla dolore laborum sunt deserunt consectetur magna. Pariatur anim duis cillum ad.\r\n",
-                        "registered": "2015-04-20T10:07:58 +04:00",
+                        "registered": "2015-04-20T10:07:58+04:00",
                         "latitude": 48.141395,
                         "longitude": -65.084242,
                         "tags": [
@@ -293,7 +293,7 @@ export default function (app) {
                         "phone": "+1 (925) 462-3800",
                         "address": "515 Sullivan Place, Grayhawk, Nevada, 937",
                         "about": "Velit duis incididunt sint quis tempor ut ullamco do. Non in ipsum do culpa culpa deserunt culpa ea laborum aute quis laborum. Cupidatat deserunt deserunt ullamco reprehenderit magna laboris nulla laborum voluptate exercitation. Enim excepteur sunt nostrud consequat non ad nostrud proident tempor. Aliquip ullamco adipisicing nisi minim. Consequat magna enim ipsum enim ipsum nulla eiusmod exercitation incididunt laborum est. Nulla adipisicing occaecat sunt officia.\r\n",
-                        "registered": "2016-09-06T11:02:08 +04:00",
+                        "registered": "2016-09-06T11:02:08+04:00",
                         "latitude": -21.098318,
                         "longitude": -10.819771,
                         "tags": [
